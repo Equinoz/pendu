10d8f690fd25ac7d2cac926a77e594eacd19f1e4
Equinoz <julien.lecoz33@gmail.com>
2021-08-16 11:39:07 +0200

    Update chokidar

--- index.js
+++ index.js
@@ -12,25 +12,3 @@ app.use(express.static(path))
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
 });
-
-// const http = require('http')
-// const fs = require('fs')
-// const httpPort = 8080
-
-// http.createServer((req, res) => {
-//   fs.readFile(__dirname + "/dist/index.html", 'utf-8', (err, content) => {
-//     if (err) {
-//       console.log('We cannot open "index.html" file.')
-//     }
-
-//     res.writeHead(200, {
-//       'Content-Type': 'text/html; charset=utf-8'
-//     })
-
-        // Doit servir les fichier statiques
-
-//     res.end(content)
-//   })
-// }).listen(httpPort, () => {
-//   console.log('Server listening on: http://localhost:%s', httpPort)
-// })
